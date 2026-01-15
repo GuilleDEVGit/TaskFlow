fbcb0ed711dfee54b356ee10f2b7ec1c7bd72ef6
GuilleDev <guillermo_g_e12@hotmail.com>
2026-01-15 19:02:04 +0100

    Visualizacion de tareas

--- src/main/java/taskflow/config/SecurityConfig.java
+++ src/main/java/taskflow/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/auth/**"
+                                "/auth/**",
+                                "/tasks/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
